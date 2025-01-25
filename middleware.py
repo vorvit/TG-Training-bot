00da3f92bc9af005578935c6b883f568b5ca730c
vorvit <vorvit@bk.ru>
2025-01-25 15:43:26 +0300

    Refactor: Stop logging to a file

--- middleware.py
+++ middleware.py
@@ -3,7 +3,7 @@ from aiogram import BaseMiddleware
 from aiogram.types import Message
 from config_reader import LOG_LEVEL
 
-logging.basicConfig(level=LOG_LEVEL)
+# logging.basicConfig(level=LOG_LEVEL)
 
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict) -> None:
@@ -12,6 +12,6 @@ class LoggingMiddleware(BaseMiddleware):
             message_text = event.text
 
             print(f"Получено сообщение: '{message_text}' от пользователя: {user_id}")
-            logging.info(f"Получено сообщение: '{message_text}' от пользователя: {user_id}")
+            # logging.info(f"Получено сообщение: '{message_text}' от пользователя: {user_id}")
 
         await handler(event, data)
